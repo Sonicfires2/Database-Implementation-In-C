59f95473cc2a425dd948b7e20f13a0adf61de121
Minh Nguyen <110623566+Sonicfires2@users.noreply.github.com>
2022-12-07 08:18:31 -0800

    Update init.h

--- init.h
+++ init.h
@@ -4,33 +4,61 @@
 #include <string.h>
 
 // Choose what kind of data structure you want to use below
-// I am using the CRD.h because I want to use pointer, the array shit is harder to use + annoying
 // File contain function for initualizing database
-
-//#include "Relation.h"
 
 #include "CRD.h"
 
 void init_HASHTABLE_CR(HASHTABLE_CR hashtable) {
     for(int i=0; i<DATABASE_SIZE; i++) {
-        hashtable[i] = new_TUPLELIST_CR("","");
+        hashtable[i] = NULL;
     }
 }
 
 void init_HASHTABLE_CP(HASHTABLE_CP hashtable) {
     for(int i=0; i<DATABASE_SIZE; i++) {
-        hashtable[i] = new_TUPLELIST_CP("","");
+        hashtable[i] = NULL;
     }
 }
 
 void init_HASHTABLE_SNAP(HASHTABLE_SNAP hashtable) {
     for(int i=0; i<DATABASE_SIZE; i++) {
-        hashtable[i] = new_TUPLELIST_SNAP("","","","");
+        hashtable[i] = NULL;
     }
 }
 
 void init_HASHTABLE_CDH(HASHTABLE_CDH hashtable) {
     for(int i=0; i<DATABASE_SIZE; i++) {
-        hashtable[i] = new_TUPLELIST_CDH("","","");
+        hashtable[i] = NULL;
+    }
+}
+
+void init_HASHTABLE_CSG(HASHTABLE_CSG hashtable) {
+    for(int i=0; i<DATABASE_SIZE; i++) {
+        hashtable[i] = NULL;
+    }
+}
+
+void init_HASHTABLE_PRO(HASHTABLE_PRO hashtable) {
+    for(int i=0; i<DATABASE_SIZE; i++) {
+        hashtable[i] = NULL;
+    }
+}
+
+void init_HASHTABLE_CNDH(HASHTABLE_CNDH hashtable) {
+    for(int i=0; i<DATABASE_SIZE; i++) {
+        hashtable[i] = NULL;
+    }
+}
+
+void init_HASHTABLE_CRDH(HASHTABLE_CRDH hashtable) {
+    for(int i=0; i<DATABASE_SIZE; i++) {
+        hashtable[i] = NULL;
+    }
+}
+
+
+void init_HASHTABLE_PRO_DAY_HOUR(HASHTABLE_PRO_DAY_HOUR hashtable) {
+    for(int i=0; i<DATABASE_SIZE; i++) {
+        hashtable[i] = NULL;
     }
 }
